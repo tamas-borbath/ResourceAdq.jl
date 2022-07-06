e8c1bf7e2ec0296925708826e7c866a4b7bcb9be
Tamás Borbáth <borbathtamas@gmail.com>
2022-07-06 17:08:26 +0200

    Added Autarky

--- src/AbstractMC/OptProblem.jl
+++ src/AbstractMC/OptProblem.jl
@@ -60,6 +60,7 @@ function OptProblem(sys::SystemModel)
         end)
         @objective(m, Min, sum(Curtailment[name]^2 for name in sys.regions.names))
     elseif haskey(sys.grid,"type") && sys.grid["type"] == "QNTC"
+        @info "Quadratic NTC"
         region_name_to_index = Dict([sys.regions.names[i] => i for i in 1:length(sys.regions.names)])
         lines_to_region = Dict(name => [] for name in sys.regions.names)
         lines_from_region = Dict(name => [] for name in sys.regions.names)
@@ -88,6 +89,36 @@ function OptProblem(sys::SystemModel)
             AvailableSupply[name in sys.regions.names], Supply[name] ≤ sum(GeneratorsCapacity[sys.generators.names[gen_index]] for gen_index in sys.region_gen_idxs[region_name_to_index[name]])
         end)
         @objective(m, Min, sum(Curtailment[name]^2 for name in sys.regions.names))
+    elseif haskey(sys.grid,"type") && sys.grid["type"] == "Autarky"
+        @info "Autarky"
+        region_name_to_index = Dict([sys.regions.names[i] => i for i in 1:length(sys.regions.names)])
+        lines_to_region = Dict(name => [] for name in sys.regions.names)
+        lines_from_region = Dict(name => [] for name in sys.regions.names)
+        for i in 1:length(sys.interfaces)
+            for i_line in sys.interface_line_idxs[i]
+                push!(lines_from_region[sys.regions.names[sys.interfaces.regions_from[i]]],sys.lines.names[i_line])
+                push!(lines_to_region[sys.regions.names[sys.interfaces.regions_to[i]]],sys.lines.names[i_line])
+            end
+        end
+
+        @variables(m, begin
+            NetPosition[name in sys.regions.names]
+            Curtailment[name in sys.regions.names] ≥ 0
+            Supply[name in sys.regions.names] ≥ 0
+            Demand[name in sys.regions.names] == 10000, Param()
+            GeneratorsCapacity[Gen in sys.generators.names] == 1000, Param()
+            LineCapacity_forward[Line in sys.lines.names] == 100, Param()
+            LineCapacity_backward[Line in sys.lines.names] == 7, Param()
+        end)
+        
+        @constraints(m, begin
+            PowerConservation, sum(NetPosition) == 0
+            NetPositionComp[name in sys.regions.names], NetPosition[name] == Supply[name] + Curtailment[name] - Demand[name]
+            ExportLimit[name in sys.regions.names], NetPosition[name] ≤ 0.0
+            ImportLimit[name in sys.regions.names], NetPosition[name] ≥ 0.0
+            AvailableSupply[name in sys.regions.names], Supply[name] ≤ sum(GeneratorsCapacity[sys.generators.names[gen_index]] for gen_index in sys.region_gen_idxs[region_name_to_index[name]])
+        end)
+        @objective(m, Min, sum(Curtailment[name] for name in sys.regions.names))
     else
         region_name_to_index = Dict([sys.regions.names[i] => i for i in 1:length(sys.regions.names)])
         lines_to_region = Dict(name => [] for name in sys.regions.names)
