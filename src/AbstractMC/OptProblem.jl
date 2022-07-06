eb674f8f11d5e20ead4b3ccc7145d01a2b20a57e
Tamás Borbáth <borbathtamas@gmail.com>
2022-07-06 14:07:29 +0200

    Added copperplate simulation type

--- src/AbstractMC/OptProblem.jl
+++ src/AbstractMC/OptProblem.jl
@@ -1,33 +1,64 @@
 function OptProblem(sys::SystemModel)
     m = Model(SOLVER.Optimizer)
     set_optimizer_attribute(m, "OutputFlag", 0)
-    region_name_to_index = Dict([sys.regions.names[i] => i for i in 1:length(sys.regions.names)])
-    lines_to_region = Dict(name => [] for name in sys.regions.names)
-    lines_from_region = Dict(name => [] for name in sys.regions.names)
-    for i in 1:length(sys.interfaces)
-        for i_line in sys.interface_line_idxs[i]
-            push!(lines_from_region[sys.regions.names[sys.interfaces.regions_from[i]]],sys.lines.names[i_line])
-            push!(lines_to_region[sys.regions.names[sys.interfaces.regions_to[i]]],sys.lines.names[i_line])
+    if haskey(sys.grid,"type") && sys.grid["type"] == "Copperplate"
+        @info "Copperplate"
+        # Line Capacities are considered infinite
+        region_name_to_index = Dict([sys.regions.names[i] => i for i in 1:length(sys.regions.names)])
+        lines_to_region = Dict(name => [] for name in sys.regions.names)
+        lines_from_region = Dict(name => [] for name in sys.regions.names)
+        for i in 1:length(sys.interfaces)
+            for i_line in sys.interface_line_idxs[i]
+                push!(lines_from_region[sys.regions.names[sys.interfaces.regions_from[i]]],sys.lines.names[i_line])
+                push!(lines_to_region[sys.regions.names[sys.interfaces.regions_to[i]]],sys.lines.names[i_line])
+            end
         end
-    end
 
-    @variables(m, begin
-        NetPosition[name in sys.regions.names]
-        Curtailment[name in sys.regions.names] ≥ 0
-        Supply[name in sys.regions.names] ≥ 0
-        Demand[name in sys.regions.names] == 10000, Param()
-        GeneratorsCapacity[Gen in sys.generators.names] == 1000, Param()
-        LineCapacity_forward[Line in sys.lines.names] == 100, Param()
-        LineCapacity_backward[Line in sys.lines.names] == 7, Param()
-    end)
-    
-    @constraints(m, begin
-        PowerConservation, sum(NetPosition) == 0
-        NetPositionComp[name in sys.regions.names], NetPosition[name] == Supply[name] + Curtailment[name] - Demand[name]
-        ExportLimit[name in sys.regions.names], NetPosition[name] ≤ sum(LineCapacity_forward[line] for line in lines_from_region[name]) + sum(LineCapacity_backward[line] for line in lines_to_region[name])
-        ImportLimit[name in sys.regions.names], NetPosition[name] ≥ -(sum(LineCapacity_backward[line] for line in lines_from_region[name]) + sum(LineCapacity_forward[line] for line in lines_to_region[name]))
-        AvailableSupply[name in sys.regions.names], Supply[name] ≤ sum(GeneratorsCapacity[sys.generators.names[gen_index]] for gen_index in sys.region_gen_idxs[region_name_to_index[name]])
-    end)
-    @objective(m, Min, sum(Curtailment[name] for name in sys.regions.names))
+        @variables(m, begin
+            NetPosition[name in sys.regions.names]
+            Curtailment[name in sys.regions.names] ≥ 0
+            Supply[name in sys.regions.names] ≥ 0
+            Demand[name in sys.regions.names] == 10000, Param()
+            GeneratorsCapacity[Gen in sys.generators.names] == 1000, Param()
+            LineCapacity_forward[Line in sys.lines.names] == 100, Param()
+            LineCapacity_backward[Line in sys.lines.names] == 7, Param()
+        end)
+        
+        @constraints(m, begin
+            PowerConservation, sum(NetPosition) == 0
+            NetPositionComp[name in sys.regions.names], NetPosition[name] == Supply[name] + Curtailment[name] - Demand[name]
+            AvailableSupply[name in sys.regions.names], Supply[name] ≤ sum(GeneratorsCapacity[sys.generators.names[gen_index]] for gen_index in sys.region_gen_idxs[region_name_to_index[name]])
+        end)
+        @objective(m, Min, sum(Curtailment[name] for name in sys.regions.names))
+    else
+        region_name_to_index = Dict([sys.regions.names[i] => i for i in 1:length(sys.regions.names)])
+        lines_to_region = Dict(name => [] for name in sys.regions.names)
+        lines_from_region = Dict(name => [] for name in sys.regions.names)
+        for i in 1:length(sys.interfaces)
+            for i_line in sys.interface_line_idxs[i]
+                push!(lines_from_region[sys.regions.names[sys.interfaces.regions_from[i]]],sys.lines.names[i_line])
+                push!(lines_to_region[sys.regions.names[sys.interfaces.regions_to[i]]],sys.lines.names[i_line])
+            end
+        end
+
+        @variables(m, begin
+            NetPosition[name in sys.regions.names]
+            Curtailment[name in sys.regions.names] ≥ 0
+            Supply[name in sys.regions.names] ≥ 0
+            Demand[name in sys.regions.names] == 10000, Param()
+            GeneratorsCapacity[Gen in sys.generators.names] == 1000, Param()
+            LineCapacity_forward[Line in sys.lines.names] == 100, Param()
+            LineCapacity_backward[Line in sys.lines.names] == 7, Param()
+        end)
+        
+        @constraints(m, begin
+            PowerConservation, sum(NetPosition) == 0
+            NetPositionComp[name in sys.regions.names], NetPosition[name] == Supply[name] + Curtailment[name] - Demand[name]
+            ExportLimit[name in sys.regions.names], NetPosition[name] ≤ sum(LineCapacity_forward[line] for line in lines_from_region[name]) + sum(LineCapacity_backward[line] for line in lines_to_region[name])
+            ImportLimit[name in sys.regions.names], NetPosition[name] ≥ -(sum(LineCapacity_backward[line] for line in lines_from_region[name]) + sum(LineCapacity_forward[line] for line in lines_to_region[name]))
+            AvailableSupply[name in sys.regions.names], Supply[name] ≤ sum(GeneratorsCapacity[sys.generators.names[gen_index]] for gen_index in sys.region_gen_idxs[region_name_to_index[name]])
+        end)
+        @objective(m, Min, sum(Curtailment[name] for name in sys.regions.names))
+    end
     return m
 end
--- test/test.jl
+++ test/test.jl
@@ -16,10 +16,20 @@ case5 = read_XLSX("test_inputs/case5/case5.xlsx")
 pm_input = PowerModels.parse_file("test_inputs/case5/case5.m")
 merge!(case5.grid,pm_input)
 
+case5.grid["type"] = "Copperpla"
+
 smallsample = AbstractMC(samples=100, seed=10234; verbose = true, threaded=true)
 @time x = assess(case5, smallsample, Shortfall());
-@show EUE(x[1])
-@show LOLE(x[1])
+println("    1: "*string(EUE(x[1],"1")))
+println("    2: "*string(EUE(x[1],"2")))
+println("    3: "*string(EUE(x[1],"3")))
+println("Total: "*string(EUE(x[1])))
+println(" ")
+
+println("    1: "*string(LOLE(x[1],"1")))
+println("    2: "*string(LOLE(x[1],"2")))
+println("    3: "*string(LOLE(x[1],"3")))
+println("Total: "*string(LOLE(x[1])))
 #=
 #smallsample = MonteCarloAPI(samples=100, seed=10233; verbose = true, threaded=false)
 
