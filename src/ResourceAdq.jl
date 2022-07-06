a9188009e6bc957c5ab3d47d19423cb946dd1eb1
Tamás Borbáth <borbathtamas@gmail.com>
2022-07-06 13:19:18 +0200

    Cleanup

--- src/ResourceAdq.jl
+++ src/ResourceAdq.jl
@@ -1,8 +1,7 @@
 module ResourceAdq
 
-using ParameterJuMP,JuMP, Gurobi
+using ParameterJuMP, JuMP, Gurobi
 const SOLVER = Gurobi
-
 
 #using PRAS
 #import PRAS: SequentialMonteCarlo, GeneratorAvailability, assess
@@ -18,23 +17,11 @@ export write_XLSX
 
 import Base: -, broadcastable, getindex, merge!
 import Base.Threads: nthreads, @spawn
-#import Dates: DateTime, Period
 import Decimals: Decimal, decimal
-#import Distributions: DiscreteNonParametric, probs, support
-#import OnlineStatsBase: EqualWeight, fit!, Mean, value, Variance
-#import OnlineStats: Series
 import Printf: @sprintf
 import Random: AbstractRNG, rand, seed!
 import Random123: Philox4x
-#import StatsBase: mean, std, stderror
-#import TimeZones: ZonedDateTime, @tz_str
 import PRAS.ResourceAdequacy: Result, ResultSpec, SimulationSpec, ResultAccumulator, MeanVariance, meanvariance, fit!, mean_std
-
-#import PRAS.ResourceAdequacy: SimulationSpec, FlowProblem, Result, ResultSpec,  ResultAccumulator, MeanVariance,  Utilization, UtilizationSamples, MinCostFlows, StorageEnergy, GeneratorStorageEnergy, StorageEnergySamples, StorageEnergySamples, GeneratorStorageEnergySamples, StorageAvailability, GeneratorStorageAvailability, LineAvailability
-#import PRAS.ResourceAdequacy: finalize, updateinjection!, solveflows!, fit!
-
-#utils 
-#import PRAS.ResourceAdequacy:mean_std, findfirstunique_directional, findfirstunique, assetgrouplist, colsum, meanvariance
 using PRAS.ResourceAdequacy
 
 
@@ -47,10 +34,7 @@ include("./ResourceAdequacy/results/results.jl")
 include("./AbstractMC/AbstractMC.jl")
 include("./PowerModelMC/PowerModelMC.jl")
 
-#Imported directly from PRAS
-#SystemModel = _P.SystemModel
 export SystemModel, SequentialMonteCarlo, assess, MonteCarloAPI, AbstractMC, PowerModelMC
 export Shortfall, Surplus, Flow, Utilization, ShortfallSamples, SurplusSamples, FlowSamples, UtilizationSamples, GeneratorAvailability
 export LOLE, EUE
-#Shortfall(), GeneratorAvailability()
 end
