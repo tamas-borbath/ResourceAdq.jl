2c5c3213ea02471a8712b18d0342beede578bae7
Tamás Borbáth <borbathtamas@gmail.com>
2022-07-13 10:37:07 +0200

    area name valdiation

--- src/validate.jl
+++ src/validate.jl
@@ -1,29 +1,26 @@
-function validate(sys::SystemModel; verbose=true)
+function validate(sys::SystemModel)
     @info "Model Validation started"
     if length(sys.grid)>1 
-        verbose && @info "Grid Model Found"
         @assert haskey(sys.grid,"gen")
-        #Generator numbers match
         @assert length(sys.generators.names) == length(sys.grid["gen"])
-        verbose && @info "Same number of generators in grid model as in inputs"
-        #generator names match
-        verbose && @info "Name corss check started"
         for (i_gen_id, i_gen) in sys.grid["gen"]
             @assert haskey(i_gen,"name")
             @assert i_gen["name"] in sys.generators.names "Generator not found in XLSX file with name: "*i_gen["name"]
         end
 
         @assert haskey(sys.grid,"branch")
-        #Branch numbers match
         @assert length(sys.lines.names) == length(sys.grid["branch"])
-        verbose && @info "Same number of branches in grid model as in inputs"
-        #generator names match
-        verbose && @info "Name corss check started"
         for (i_obj_id, i_obj) in sys.grid["branch"]
             @assert haskey(i_obj,"name")
             @assert i_obj["name"] in sys.lines.names "Line not found in XLSX file with name: "*i_obj["name"]
         end
-        verbose && @info "Name cross check finished"
+
+        @assert haskey(sys.grid,"area_name")
+        @assert length(sys.regions.names) == length(sys.grid["area_name"])
+        for (i_obj_id, i_obj) in sys.grid["area_name"]
+            @assert haskey(i_obj,"name")
+            @assert i_obj["name"] in sys.regions.names "Region not found in XLSX file with name: "*i_obj["name"]
+        end
     end 
     @info "Modal Validation finished"
     return true
